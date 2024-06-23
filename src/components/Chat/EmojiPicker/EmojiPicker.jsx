e63ac0a5897f0812a57d4af16ab345b9efc5f5f2
Thiago Campos <thiagoabcfc@hotmail.com>
2024-06-23 00:28:17 -0300

    fix: position emoji

--- src/components/Chat/EmojiPicker/EmojiPicker.jsx
+++ src/components/Chat/EmojiPicker/EmojiPicker.jsx
@@ -1,13 +1,24 @@
 /* eslint-disable react/prop-types */
 import Picker from 'emoji-picker-react';
 
-const EmojiPicker = ({ visible, position, onClose, onEmojiClick }) => {
+const EmojiPicker = ({ visible, onClose, onEmojiClick }) => {
   if (!visible) return null;
 
+  // Calcula a posição correta para centralizar horizontalmente o seletor de emoji
+  const pickerStyle = {
+      top: '25%',  // Posiciona 10px abaixo do botão
+      left: '10%',               // Centraliza horizontalmente
+      transform: 'translateX(-50%)',  // Centraliza horizontalmente
+      maxWidth: '40px',         // Define uma largura máxima para o modal
+      boxShadow: '0px 8px 16px rgba(0, 0, 0, 0.1)',  // Sombra
+      borderRadius: '8px',       // Bordas arredondadas
+  
+  };
+
   return (
-    <div className="emoji-picker absolute z-50" style={{ top: position.top, left: position.left }}>
+    <div className="emoji-picker absolute bg-white p-2 rounded-lg shadow-md" style={pickerStyle}>
       <Picker onEmojiClick={onEmojiClick} />
-      <button className="bg-blue-500 text-white px-4 py-2 mt-2 rounded-lg shadow-md hover:bg-blue-600 focus:outline-none" onClick={onClose}>Fechar</button>
+      <button className="bg-green-500 text-white px-3 py-1 mt-1 rounded-md shadow-md hover:bg-green-600 focus:outline-none text-sm" onClick={onClose}>Fechar</button>
     </div>
   );
 };
