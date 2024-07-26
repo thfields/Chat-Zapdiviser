bf948873c7c01305bff3e25c6969c7db15f61d2b
thfields <thiagoabcfc@hotmail.com>
2024-07-26 20:00:00 -0300

    create mocks

---
+++ src/data/Mocks.js
@@ -0,0 +1,10 @@
+export const contactProfileImages = {
+    Vitor: "/src/assets/vitor.png",
+    Rickson: "/src/assets/sem-foto.png",
+    Thiago: "/src/assets/thiago.png",    
+    Marcos: "/src/assets/marcos.png",
+    Rodrigo: "/src/assets/rodrigo.png",
+    Abner: "/src/assets/sem-foto.png",
+};
+
+export const initialContacts = ["Vitor", "Rickson", "Thiago", "Marcos", "Rodrigo", "Abner"];
